2778e75dcd026699a13a1e44c8cfba13ce8875c4
sahilsapariya <sahilsapariya03@gmail.com>
2025-03-04 23:53:25 +0530

    feat: infinite command loop

--- src/builtin.c
+++ src/builtin.c
@@ -6,17 +6,6 @@ void print_prompt() {
     printf(PROMPT_MSG);
 }
 
-void exit_shell() {
-    char user_input;
-    printf("\nLeaving early? Are you sure you want to exit? (y/N)\n");
-    scanf("%c", &user_input);
-
-    if (user_input == 'y') {
-        printf("Exiting shell...\n");
-        exit(0);
-    }
-}
-
 void read_input(char *input) {
     fgets(input, 1024, stdin);
     return;
@@ -25,4 +14,19 @@ void read_input(char *input) {
 void display_output(char *output) {
     printf("%s", output);
     return;
+}
+
+void exit_shell() {
+    char user_input;
+    printf("\nLeaving early? Are you sure you want to exit? (y/N)\n");
+    scanf("%c", &user_input);
+
+    // Clear new line character left in input buffer,
+    // it prevents the printing 2 times the prompt message
+    while (getchar() != '\n');
+
+    if (user_input == 'y') {
+        printf("Exiting shell...\n");
+        exit(0);
+    }
 }
--- src/main.c
+++ src/main.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<string.h>
 #include "builtin.h"
 
 int main() {
     char cwd[1024];
     getlogin_r(cwd, sizeof(cwd));
 
-    print_prompt();
+    do {
+        print_prompt();
+        read_input(cwd);
 
-    read_input(cwd);
-    display_output(cwd);
+        // check if user wants to exit
+        if (strcmp(cwd, "exit\n") == 0) {
+            exit_shell();
+        }
 
-    exit_shell();
+        display_output(cwd);
+    } while (1);
 
     return 0;
 }
